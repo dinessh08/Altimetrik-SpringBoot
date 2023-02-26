08c29a03743b85d8217e0bd806e97e9ee46847ef
drajasekaran4814 <dinesshprabu@gmail.com>
2023-02-26 13:32:34 +0530

    05 Init COmmit for Config server changes

---
+++ Config-Server/src/test/java/com/dinessh/altiemtrik/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinessh.altiemtrik.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
